cdbcf360c1d41c99d54e05f8c330b1309bcb8e8f
Mathias <liesenborghsmemploi@gmail.com>
2024-11-05 13:33:42 +0100

    WIP ennemies spawn

--- Assets/Scripts/MathiasDescent.cs
+++ Assets/Scripts/MathiasDescent.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class MathiasDescent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float speed;
+
+    public void setSpeed(float newSpeed)
     {
-        
+        speed = newSpeed;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position += Vector3.down * speed * Time.deltaTime;
+
+        if (transform.position.y < -Screen.height / 2f)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/MathiasSpawner.cs
+++ Assets/Scripts/MathiasSpawner.cs
@@ -4,41 +4,67 @@ using UnityEngine;
 
 public class MathiasSpawner : MonoBehaviour
 {
-    public GameObject objectPrefab;
-    public float spawnRateMin = 1f;
-    public float spawnRateMax = 1f;
-    public float objectSpeed = 2f;
-    public float spawnHeight = 10f;
+    public GameObject objectPrefab; 
+    public float spawnRateMin = 1f; 
+    public float spawnRateMax = 3f; 
+    public float objectSpeed = 2f; 
+    public float spawnHeight = 10f; 
 
-    private float timeToNextSpawn = 0f;
 
-    void Start()
+    private void Start()
     {
-        timeToNextSpawn = Random.Range(spawnRateMin, spawnRateMax);
+        
+        StartCoroutine(SpawnObjects());
     }
 
-     void Update()
+    
+    private IEnumerator SpawnObjects()
     {
-        timeToNextSpawn -= Time.deltaTime;
-
-        if (timeToNextSpawn < 0f)
+        while (true)
         {
-            timeToNextSpawn = Random.Range(spawnRateMin, spawnRateMax);
+           
+            yield return new WaitForSeconds(Random.Range(spawnRateMin, spawnRateMax));
+
+            
+            SpawnObject();
         }
     }
 
-    void SpawnObject()
+    private void SpawnObject()
     {
-        //setting to spawn object on x axis
-
+        
         float randomX = Random.Range(-Screen.width / 2f, Screen.width / 2f);
         Vector3 spawnPosition = new Vector3(randomX, spawnHeight, 0f);
 
+        
         Vector3 worldPos = Camera.main.ScreenToWorldPoint(spawnPosition);
-        worldPos.z = 0f;
+        worldPos.z = 0f; 
 
+        
         GameObject spawnedObject = Instantiate(objectPrefab, worldPos, Quaternion.identity);
 
+        
+        StartCoroutine(MoveObjectDown(spawnedObject));
+    }
 
+    
+    private IEnumerator MoveObjectDown(GameObject obj)
+    {
+        
+        Vector3 startPosition = obj.transform.position;
+
+        
+        float endY = -Screen.height / 2f;
+
+        
+        while (obj.transform.position.y > endY)
+        {
+            
+            obj.transform.position += Vector3.down * objectSpeed * Time.deltaTime;
+            yield return null; 
+        }
+
+        
+        Destroy(obj);
     }
 }
